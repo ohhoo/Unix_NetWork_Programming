c9b1f880f8a0f79d67a31eadf3b5c020e0f575b3
ohhoo <xudong1997124@outlook.com>
2022-02-27 11:49:21 +0800

    [MOD]按照章节习题1.5修改了程序

--- Introduction/timeClient.cpp
+++ Introduction/timeClient.cpp
@@ -20,7 +20,8 @@ int main(int argc, char **argv) {
     //执行清除操作，将servaddr结构初始化为0
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
-    servaddr.sin_port   = htons(13);//得到的端口号为3328
+    //servaddr.sin_port   = htons(13);//得到的端口号为3328
+    servaddr.sin_port   = htons(9999);
 
     //将接收到的点分十进制IP地址转换为二进制整数
     if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
@@ -34,14 +35,16 @@ int main(int argc, char **argv) {
 
     printf("port = %d\n",servaddr.sin_port);
 
+    int count = 0;
     //从套接字内读取数据，当read返回值为0或负数时读取结束
     while ((n = read(sockfd, recvline, MAXLINE)) > 0) {
         recvline[n] = 0;
+        ++count;
         if (fputs(recvline, stdout) == EOF) {
             err_sys("fputs error");
         }
     }
-    
+    printf("read > 0:%d\n",count);
     if (n < 0) {
         err_sys("read error");
     }
--- Introduction/timeServer.cpp
+++ Introduction/timeServer.cpp
@@ -17,23 +17,29 @@ int main(int argc, char **argv)
 	servaddr.sin_family      = AF_INET;
 	//指定IP地址为INADDR_ANY,这样如果服务器主机有多个接口，服务器进程就可以在任意网络接口上接受连接
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port        = htons(13);//端口要与timeClient中要连接的端口一致
+	//servaddr.sin_port        = htons(13);//端口要与timeClient中要连接的端口一致
+	servaddr.sin_port        = htons(9999);
 
 	//将端口绑定至创建的socket
 	bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
 
-	//将创建的socket转换为监听socket，LISTENQ表示队列中等待连接最大的连接数
+	//将创建的socket转换为监听socket，LISTENQ表示队列中允许容纳等待连接最大的连接数
 	listen(listenfd, LISTENQ);
 
-	for(;;) {//持续等待连接、写入数据，完成后断开连接等待下一次连接建立
+	for(;;) {//持续等待建立连接、写入数据，完成后断开连接等待下一次连接建立
 		//建立连接，接受连接的过程由内核完成
 		connfd = accept(listenfd, (SA *) NULL, NULL);
 
         ticks = time(NULL);
         snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
-        //写入数据
-		write(connfd, buff, strlen(buff));
-
+        //一次性写入数据
+		//write(connfd, buff, strlen(buff));
+		//循环写入
+		int buffLen = strlen(buff);
+		int len = 0;
+		while(len++<buffLen){
+			write(connfd,buff+len,1);
+		}
 		//关闭连接
 		close(connfd);
 	}
